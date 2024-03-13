a96a9e843f398a80aa47013aadb442d23a197fdc
fs0a <162139702+fs0a@users.noreply.github.com>
2024-03-13 23:35:11 +0700

    reupload 13 maret

--- js/script.js
+++ js/script.js
@@ -1,68 +1,87 @@
 
+ 
+    
+function checkform(){
+    var weight, height, age, gender;
+    weight = document.getElementById("hw").value //get weight value
+    height = document.getElementById("hh").value // get height value
+    age = document.getElementById("ha").value // get age value
+    gender = document.getElementsByName('sex')// get selected sex
+    
+    for (i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            
+        if (gender[i].value == "Lady"){
+            if (age < 1){ 
+                alert("Please fill out age");
+            } else if (age < 18){ 
+                document.getElementById("resultSX").innerHTML = "Girl";
+                calculateBMI();
+            } else if (age < 30) { 
+                document.getElementById("resultSX").innerHTML = "Lady";
+                calculateBMI();
+            } else if (age < 120) { 
+                document.getElementById("resultSX").innerHTML = "Ma'am";
+                calculateBMI();
+            } else {
+                document.getElementById("resultSX").innerHTML = "Ancient One";
+                calculateBMI();
+            }
+        } else if (gender[i].value == "Sir"){
+            if (age < 1){ 
+                alert("Please fill out age");
+            } else if (age < 18){ 
+                document.getElementById("resultSX").innerText = "Boy";
+                
+                calculateBMI();
+            } else if (age < 30) { 
+                document.getElementById("resultSX").innerHTML = "Mister";
+                calculateBMI();
+            } else if (age < 120) { 
+                document.getElementById("resultSX").innerHTML = "Sir";
+                
+                calculateBMI();
+            } else {
+                document.getElementById("resultSX").innerHTML = "Ancient One";
+                calculateBMI();
+            }
+        }        
+    }    }
+}
 
 function calculateBMI() {
-
-var weight, height, age, gender;
-weight = document.getElementById("hw").value //get weight value
-height = document.getElementById("hh").value // get height value
-age = document.getElementById("ha").value // get age value
-gender = document.getElementsByName('sex')// get selected sex
-var call = String     ; //set nickname
+    var weight, height, age, gender;
+    weight = document.getElementById("hw").value //get weight value
+    height = document.getElementById("hh").value // get height value
+    age = document.getElementById("ha").value // get age value
+    gender = document.getElementsByName('sex')// get selected sex
     
-
-    for (i = 0; i < gender.length; i++) {
-    if (gender[i].checked) {
-        
-    if (gender[i].value == "Lady"){
-        if (age < 18){ 
-            document.getElementById("resultSX").innerHTML = "Girl";
-        } else if (age < 30) { 
-            document.getElementById("resultSX").innerHTML = "Lady";
-        } else if (age < 120) { 
-            document.getElementById("resultSX").innerHTML = "Ma'am";
-        } else {
-            document.getElementById("resultSX").innerHTML = "Ancient One";
-        }
-    } else  {
-        if (age < 18){ 
-            document.getElementById("resultSX").innerHTML = "Boy";
-        } else if (age < 30) { 
-            document.getElementById("resultSX").innerHTML = "Mister";
-        } else if (age < 120) { 
-            document.getElementById("resultSX").innerHTML = "Sir";
-        } else {
-            document.getElementById("resultSX").innerHTML = "Ancient One";
-        }
-    }
-
-
-//gender = document.querySelector('input[name="sex"]:checked').value;
-//[i]
-//(gender.value[i] = "Sir")
-
-var BMIVal = parseInt(weight * 10000/ (height * height));   
+    
+    var BMIVal = parseInt(weight * 10000/ (height * height));   
 
 
     document.getElementById("BMIval").innerHTML = BMIVal;
+    
 
     if (30 <= BMIVal){
         document.getElementById("BMIcat").innerHTML = "Obese";
-        document.getElementById("BMI-exp").innerHTML = gender[i].value + ", it is for the best of yourself to lose some weight"; 
+        document.getElementById("BMI-exp").innerHTML =  "it is for the best of yourself to lose some weight"; 
     } else if (25 <= BMIVal & BMIVal < 30){
     document.getElementById("BMIcat").innerHTML = "Overweight";
-    document.getElementById("BMI-exp").innerHTML = gender[i].value + ", it is for the better of yourself to lose some weight";
+    document.getElementById("BMI-exp").innerHTML = "it is for the better of yourself to lose some weight"
     } else if ( 18 <= BMIVal & BMIVal < 25) {
         document.getElementById("BMIcat").innerHTML = "Normal";
-        document.getElementById("BMI-exp").innerHTML = gender[i].value + ", you are doing really good. Please maintain this lifestyle for longer future";
+        document.getElementById("BMI-exp").innerHTML = "you are doing really good. Please maintain this lifestyle for longer future";
     } else if(BMIVal < 18){
         document.getElementById("BMIcat").innerHTML = "Underweight";
-        document.getElementById("BMI-exp").innerHTML = gender[i].value + ", it is for the better of yourself to gain some weight";
+        document.getElementById("BMI-exp").innerHTML = "it is for the better of yourself to gain some weight";
     }
 
-    }
+    
  }
-}
- 
+
+
+
 function resetBMI() {
     document.getElementById("BMIval").innerHTML = "thank you";
     document.getElementById("BMIcat").innerHTML = "you have removed your data";
